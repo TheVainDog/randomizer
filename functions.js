7f6b740ec4fb89675c19adbca426aea658b8082c
TheVainDog <yanks_0_2@hotmail.com>
2021-11-24 16:43:24 -0600

    version 1

--- functions.js
+++ functions.js
@@ -1,70 +1,69 @@
-let player = "00";
-let toIndex = 0;
 let fromIndex = 0;
-let arrFrom = ['01', '02', '03', '04', '05', '06', '07', '08', '09',];
-let arrTo = ['01', '02', '03', '04', '05', '06', '07', '08', '09',];
+let fromPlayer = "00";
+let arrFrom = ['01', '02', '03', '04', '05', '06', '07', '08', '09'];
 
-function test() {
-    console.log(arrFrom);
-    arrFrom.splice(1,1);
-    console.log(arrFrom);
-}
+let toIndex   = 0;
+let toPlayer   = "00";
+let arrTo   = ['01', '02', '03', '04', '05', '06', '07', '08', '09'];
 
-function from() {
-    let initialDelay = 3000;
-    if (arrFrom.length == 0){
-        console.log("NO MORE arrFrom");
-        return;
-    }
-    if (arrFrom.length == 1){
+const timer = ms => new Promise(res => setTimeout(res, ms));
+
+async function from() {
+    
+    if (arrFrom.length == 0) return;
+
+    if (arrFrom.length == 1) {
+        document.getElementById("coin").play();
         document.getElementById("up_"+arrFrom[0]).classList.remove('bg-primary');
         document.getElementById("up_"+arrFrom[0]).classList.add('bg-danger');
+        fromPlayer = arrFrom[0];
         arrFrom.splice(0,1);
         return;
     }
-    while(initialDelay > 0){
-        setTimeout(() => {
-            fromIndex = Math.floor(Math.random() * arrFrom.length);
-            document.getElementById("turno").value = arrFrom[fromIndex];
-            player = arrFrom[fromIndex];
-        }, initialDelay);
-        initialDelay--;
-    }
-    setTimeout(() => {
+    
+    document.getElementById("box").play();
+    for (let delay = 1; delay <= 100; delay+=1) {
+        fromIndex = Math.floor(Math.random() * arrFrom.length);
+        fromPlayer = arrFrom[fromIndex];
+        document.getElementById("turno").value = arrFrom[fromIndex];
         document.getElementById("up_"+arrFrom[fromIndex]).classList.remove('bg-primary');
         document.getElementById("up_"+arrFrom[fromIndex]).classList.add('bg-danger');
-        arrFrom.splice(fromIndex,1);
-    }, 3010);
-}
-
-function to(){
-    if (player == "00") return;
-
-    let toPlayer = "00";
-    
-    let initialDelay = 3000;
-    //arrTo.splice(arrTo.indexOf(player),1);
-    console.log(arrTo);
-    
-    toIndex = Math.floor(Math.random() * arrTo.length);
-
-    while(initialDelay > 0){
-        setTimeout(() => {
-            toIndex = Math.floor(Math.random() * arrTo.length);
-            document.getElementById("down_"+player).value = arrTo[toIndex];
-            toPlayer = arrTo[toIndex];
-        }, initialDelay);
-        initialDelay--;
+        await timer(delay);
+        document.getElementById("up_"+arrFrom[fromIndex]).classList.remove('bg-danger');
+        document.getElementById("up_"+arrFrom[fromIndex]).classList.add('bg-primary');
     }
-    setTimeout(() => {
-        document.getElementById("down_"+player).classList.remove('bg-warning');
-        document.getElementById("down_"+player).classList.remove('text-dark');
-        document.getElementById("down_"+player).classList.add('bg-success');
-        document.getElementById("down_"+player).classList.add('text-white');
-        document.getElementById("down_"+player).value = toPlayer;
-        arrTo.splice(toIndex,1);
-        console.log(arrTo);
-    }, 3010);
-
+    document.getElementById("coin").play();
+    document.getElementById("up_"+arrFrom[fromIndex]).classList.remove('bg-primary');
+    document.getElementById("up_"+arrFrom[fromIndex]).classList.add('bg-danger');
+    arrFrom.splice(fromIndex,1);
 }
 
+
+async function to() {
+    if (arrTo.length == 0) return;
+
+    if (arrTo.length == 1) {
+        document.getElementById("kids").play();
+        document.getElementById("down_"+fromPlayer).classList.remove('bg-warning');
+        document.getElementById("down_"+fromPlayer).classList.remove('text-dark');
+        document.getElementById("down_"+fromPlayer).classList.add('bg-success');
+        document.getElementById("down_"+fromPlayer).classList.add('text-white');
+        document.getElementById("down_"+fromPlayer).value = arrTo[0];
+        arrTo.splice(0,1);
+        return;
+    }
+        
+    document.getElementById("box").play();
+    for (let delay = 1; delay <= 100; delay+=1) {
+        toIndex = Math.floor(Math.random() * arrTo.length);
+        toPlayer = arrTo[toIndex];
+        document.getElementById("down_"+fromPlayer).value = arrTo[toIndex];
+        await timer(delay);
+    }
+    document.getElementById("kids").play();
+    document.getElementById("down_"+fromPlayer).classList.remove('bg-warning');
+    document.getElementById("down_"+fromPlayer).classList.remove('text-dark');
+    document.getElementById("down_"+fromPlayer).classList.add('bg-success');
+    document.getElementById("down_"+fromPlayer).classList.add('text-white');
+    arrTo.splice(toIndex,1);
+}
